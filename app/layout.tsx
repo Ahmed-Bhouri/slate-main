2fdd89d6ace8e89fac2b7308985ea55812dd7bf5
Ahmed-Bhouri <ahmed.bhouri@gmail.com>
2026-02-28 01:24:12 +0100

    shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, DM_Sans } from "next/font/google";
 import "./globals.css";
+
+const dmSans = DM_Sans({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={dmSans.variable}>
       <body className={inter.className}>{children}</body>
     </html>
   );
